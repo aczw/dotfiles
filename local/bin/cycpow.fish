7e94be2759b8273f552799370990970683ff571f
ash <charleszw16@gmail.com>
2023-03-20 06:51:21 -0400

    fish: simplify power profile script

--- local/bin/cycpow.fish
+++ local/bin/cycpow.fish
@@ -1,16 +1,13 @@
 #!/usr/bin/fish
 
-# if no arguments were given, we simply cycle to next profile.
-# otherwise, we switch to the specified profile.
+# if no arguments are given, we simply cycle to next profile.
+# otherwise, we check if specified profile is correct, and switch to it
 if test (count $argv) -lt 1
     asusctl profile -n
 else
-    asusctl profile -P $argv[1]
-    if test $status -ne 0
+    if not asusctl profile -P $argv[1]
         return 1
     end
 end
 
-# deliver noti about new power profile
 notify-send -t 3000 "asusctl" "$(asusctl profile -p)"
-
